584b6e90f930172022609f5d116a13c698cc587c
garyguo110 <garyguo110@gmail.com>
2020-07-21 20:01:51 -0700

    Add dynamic port

--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var app = express();
+const port = process.env.PORT || 8000;
 
 app.get('/', function (req, res) {
     res.send('Hello World');
  })
  
- var server = app.listen(8000, function () {
+ var server = app.listen(port, function () {
     var port = server.address().port
     
     console.log("Example app listening at port %s", port)
