e2d8ab23015f8abb76e7d83c189412f5902ba575
garyguo110 <garyguo110@gmail.com>
2020-08-13 08:03:04 -0700

    Update Court Structure

---
+++ database/allCourts.sql
@@ -0,0 +1,8 @@
+INSERT INTO tennis_courts VALUES ('4438853', 'Alice Marble Tennis Court #1', 'Greenwich St & Hyde St, San Francisco, CA');
+INSERT INTO tennis_courts VALUES ('4433542', 'Alice Marble Tennis Court #2', 'Greenwich St & Hyde St, San Francisco, CA');
+INSERT INTO tennis_courts VALUES ('3333206', 'Alice Marble Tennis Court #3', 'Greenwich St & Hyde St, San Francisco, CA');
+INSERT INTO tennis_courts VALUES ('3333250', 'Alice Marble Tennis Court #4', 'Greenwich St & Hyde St, San Francisco, CA');
+INSERT INTO tennis_courts VALUES ('3333199', 'Balboa Park Tennis Court #1', 'Ocean Ave & San Jose Ave, San Francisco, CA');
+INSERT INTO tennis_courts VALUES ('3333256', 'Balboa Park Tennis Court #2', 'Ocean Ave & San Jose Ave, San Francisco, CA');
+INSERT INTO tennis_courts VALUES ('4456695', 'Balboa Park Tennis Court #3', 'Ocean Ave & San Jose Ave, San Francisco, CA');
+INSERT INTO tennis_courts VALUES ('4456807', 'Balboa Park Tennis Court #4', 'Ocean Ave & San Jose Ave, San Francisco, CA');
