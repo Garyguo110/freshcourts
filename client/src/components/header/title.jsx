892dfdce00343fca3084459fca061a14abb34a24
joshgrg <georgj7@mcmaster.ca>
2020-10-20 21:52:19 -0400

    Header of Front page added

---
+++ client/src/components/header/title.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Title = () => {
+    return ( 
+        <div className="column is-one-third">
+            <h1 className="title is-1 has-text-white center">FreshCourts</h1>
+            <h2 className="subtitle is-2 has-text-white center">Play The Game You Love</h2>
+        </div>
+     );
+}
+ 
+export default Title;
