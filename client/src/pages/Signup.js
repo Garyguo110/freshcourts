d729589648f70fff27c15038235b1b0745102b12
joshgrg <georgj7@mcmaster.ca>
2020-10-22 22:08:42 -0400

    added styling to signup page

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -10,23 +10,23 @@ class Signup extends Component {
             <div className="form-wrapper">
                 <form onSubmit={this.handleSubmit} noValidate>
                     <div className="firstName">
-                        <label htmlFor="firstName">First Name</label>
-                        <input type="text" className="" placeholder="First Name" name="firstName" noValidate onChange={this.handleChange}></input>
+                        <label className="subtitle is-5 mb-0 has-text-black" htmlFor="firstName">First Name</label>
+                        <input type="text" className="subtitle is-6" placeholder="First Name" name="firstName" noValidate onChange={this.handleChange}></input>
                     </div>
                     <div className="lastName">
-                        <label htmlFor="lastName">Last Name</label>
-                        <input type="text" className="" placeholder="Last Name" name="lastName" noValidate onChange={this.handleChange}></input>
+                        <label className="subtitle is-5 mb-0 has-text-black" htmlFor="lastName">Last Name</label>
+                        <input type="text" className="subtitle is-6" placeholder="Last Name" name="lastName" noValidate onChange={this.handleChange}></input>
                     </div>
                     <div className="email">
-                        <label htmlFor="email">Email</label>
-                        <input type="email" className="" placeholder="Email" name="email" noValidate onChange={this.handleChange}></input>
+                        <label className="subtitle is-5 mb-0 has-text-black" htmlFor="email">Email</label>
+                        <input type="email" className="subtitle is-6" placeholder="Email" name="email" noValidate onChange={this.handleChange}></input>
                     </div>
                     <div className="password">
-                        <label htmlFor="password">Password</label>
-                        <input type="password" className="" placeholder="Password" name="password" noValidate onChange={this.handleChange}></input>
+                        <label className="subtitle is-5 mb-0 has-text-black" htmlFor="password">Password</label>
+                        <input type="password" className="subtitle is-6" placeholder="Password" name="password" noValidate onChange={this.handleChange}></input>
                     </div>
                     <div className="createAccount">
-                        <button>Continue</button>
+                        <button className="subtitle is-5 mb-2 has-text-white has-text-weight-medium">Continue</button>
                         <small>Already Have an Account?</small>
                     </div>
                 </form>
